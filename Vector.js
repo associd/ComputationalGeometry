71f37c5eb1c701e73dbd4be42b402e08ac9b15cb
xyz <1351998071@qq.com>
2024-07-09 18:30:54 +0800

    第一，第二的极点的寻找

--- Vector.js
+++ Vector.js
@@ -1,26 +1,90 @@
 class Vector2 {
     constructor(x, y) {
-        this.id = -1;
         this.x = x
         this.y = y
         this.size = 4;
-        this.color = "black";
+        this.color = "white";
     }
 
     Substract = (vector) => {
         return new Vector2(this.x - vector.x, this.y - vector.y)
     }
 
+    Divide = (number) => {
+        return new Vector2(this.x / number, this.y / number)
+    }
+
+    Multiple = (number) => {
+        return new Vector2(this.x * number, this.y * number)
+    }
+
+    Normalize = () => {
+        let magnitude = Math.sqrt(this.x * this.x + this.y * this.y)
+        return magnitude > 0 ? this.Divide(magnitude) : Vector2.Zero;
+    }
+
+    Draw = (ctx) => {
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI)
+        ctx.fillStyle = this.color
+        ctx.fill()
+    }
 
     static Cross = (l, r) => {
         return l.x *　r.y - r.x * l.y;
     }
 
     static ToLeft = (vectorA, vectorB, vectorS) => {
+        // 因canvas y 轴朝下 则toleft测试为真时 实际点在线段的右边
         return Vector2.Cross(
             vectorA.Substract(vectorS),
             vectorB.Substract(vectorS)
-        )
+        ) < 0
+    }
+
+    static Zero = new Vector2(0 , 0)
+}
+
+class Segment {
+    constructor(vector2Start, vector2End) {
+        this.start = vector2Start;
+        this.end = vector2End;
+        this.lineWidth = 2
+        this.color = "white"
+
+        this.path = new Path2D();
+        this.path.moveTo(this.start.x, this.start.y)
+        this.path.lineTo(this.end.x, this.end.y)
+    }
+
+    Draw = (ctx) => {
+        ctx.save()
+        ctx.strokeStyle = this.color
+        ctx.lineWidth = this.lineWidth
+        ctx.stroke(this.path)
+
+        let arrow = this.start.Substract(this.end).Normalize().Multiple(8)
+
+        ctx.translate(this.end.x, this.end.y)
+        ctx.rotate(Math.PI / 4)
+        ctx.moveTo(0, 0)
+        ctx.lineTo(arrow.x, arrow.y)
+        ctx.stroke();
+
+        ctx.rotate(-Math.PI / 2)
+        ctx.moveTo(0, 0)
+        ctx.lineTo(arrow.x, arrow.y)
+        ctx.stroke();
+
+        ctx.rotate(Math.PI / 4)
+        ctx.translate(-this.end.x, -this.end.y)
+
+        this.start.Draw(ctx)
+        ctx.restore();
+    }
+
+    PointInPath = (ctx, x, y) => {
+        return ctx.isPointInStroke(this.path, x, y)
     }
 }
 
@@ -33,6 +97,23 @@ class Convex {
         this.SortByAngle();
     }
 
+    Draw = (ctx) => {
+        this.verteis.forEach((point, index) => {
+            point.Draw(ctx)
+            if (point.id !== -1)
+            {
+                ctx.beginPath()
+                ctx.font = "16px serif"
+                ctx.strokeStyle = "#ffffff"
+                ctx.strokeText(index, point.x + point.size / 2, point.y - point.size / 2, 28)
+                ctx.stroke()
+            }
+        })
+        this.extremies.forEach(point => {
+            point.Draw(ctx)
+        })
+    }
+
     AddPoint = (v) => {
         this.verteis.push(v);
         this.GetFirstExtreme();
@@ -41,6 +122,9 @@ class Convex {
 
     GetFirstExtreme = () => {
         if (this.verteis.length === 0) return  undefined;
+
+        this.extremies.splice(0, this.extremies.length);
+
         this.firstExtreme = this.verteis[0];
         for (let i = 0; i < this.verteis.length; i++) {
             this.verteis[i].color = Convex.DefaultColor;
@@ -51,23 +135,24 @@ class Convex {
         }
         this.firstExtreme.color = "#00b2ff";
 
-        if (!this.extremies.includes(this.firstExtreme)){
-            this.extremies.push(this.firstExtreme);
-        }
+        this.verteis.splice(this.verteis.indexOf(this.firstExtreme), 1)
+        this.verteis.unshift(this.firstExtreme)
+        this.extremies.push(this.firstExtreme);
     }
 
     SortByAngle = () => {
         this.verteis.sort((a, b) => {
-            if (a !== this.firstExtreme && a !== b && Vector2.ToLeft(this.firstExtreme, a, b)){
-                return 1
+            if (Vector2.ToLeft(this.firstExtreme, a, b)){
+                return -1
             }
             else{
-                return -1;
+                return 1;
             }
         })
-        this.verteis.forEach((v, i) => {
-            v.id = i;
-        })
+        if (this.verteis.length > 1) {
+            this.verteis[1].color = "#00b2ff";
+            this.extremies[1] = this.verteis[1]
+        }
     }
 
     static DefaultColor = "#12ff00";
